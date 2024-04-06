d3b651b27449268b317ff7a93350f827eddec4d4
amiinmuse <amiinmuse2025@u.northwestern.edu>
2024-04-06 15:33:55 -0500

    added pages

---
+++ pages/courses.tsx
@@ -0,0 +1,15 @@
+
+
+export default function Courses() {
+    return (
+      <div>
+        <h1>Sample Courses</h1>
+        <ul>
+          <li>Web Development</li>
+          <li>Data Science</li>
+          <li>Artificial Intelligence</li>
+          <li>Cloud Computing</li>
+        </ul>
+      </div>
+    );
+  }
