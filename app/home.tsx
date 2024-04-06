a4b835fa17e343f9b108f2b5c8b189bfe4fcbc19
amiinmuse <amiinmuse2025@u.northwestern.edu>
2024-04-06 14:52:20 -0500

    changed home page

--- app/home.tsx
+++ app/home.tsx
@@ -1,9 +1,8 @@
-
+import { UserButton } from "@clerk/nextjs";
+import Image from "next/image";
 
 export default function Home() {
-    return (
-        <h1> Home </h1>
-    )
+  return (
+    <h1> Home </h1>
+  );
 }
-
-
