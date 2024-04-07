a67554c48ca36976227d10ae2b28307d697a4346
Herbert540 <HerbertBotwe2025@u.northwestern.edu>
2024-04-07 15:17:43 -0500

    as

--- app/page.tsx
+++ app/page.tsx
@@ -4,22 +4,38 @@ import Link from 'next/link';
 import TopBar from '../components/TopBar';
 import { UserButton } from "@clerk/nextjs";
 import styles from '../styles/Home.module.css';
-import { useEffect, useState } from 'react';
+import { FormEvent, useEffect, useState } from 'react';
 
 const Home = () => {
 
   const [courses, setCourses] = useState([]);
+  const [newCourseName, setNewCourseName] = useState(""); // Step 1
 
   useEffect(() => {
-    const fetchCourses = async () => {
-      const response = await fetch('http://localhost:3002/courses');
-      console.log(response);
-      const data = await response.json();
-      setCourses(data);
-    };
-
     fetchCourses();
   }, []);
+
+  const fetchCourses = async () => {
+    const response = await fetch('http://localhost:3002/courses');
+    console.log(response);
+    const data = await response.json();
+    setCourses(data);
+  };
+
+  const handleAddCourse = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault(); // Prevent the form from submitting in the traditional way
+    const response = await fetch('http://localhost:3002/courses', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ course_name: newCourseName }),
+    });
+    if (response.ok) {
+      setNewCourseName(""); // Reset input field
+      await fetchCourses(); // Re-fetch courses or update state directly
+    }
+  };
 
   return (
     <>
@@ -30,17 +46,14 @@ const Home = () => {
           A <strong>simple platform</strong> that gives you a <strong>step-by-step guide</strong> to get you started<br />
           with daunting <strong>class assignments</strong>!
         </p>
-        {/* <div className={styles.buttonContainer}>
-          <Link href="/courses" className={styles.button} passHref>Courses</Link>
-        </div> */}
-
-        {/* <div className={styles.coursesContainer}>
-          {courses.map((course: { _id: string, course_name: string }) => (
-            <Link className={styles.courseCard} key={course._id} href={`/courses/${course._id}`} passHref>
-              <h3>{course.course_name}</h3>
-            </Link>
-          ))}
-        </div> */}
+        {/* <form onSubmit={(event) => handleAddCourse(event)}>
+          <input
+            value={newCourseName}
+            onChange={(e) => setNewCourseName(e.target.value)}
+            placeholder="New course name"
+          />
+          <button type="submit">Add Course</button>
+        </form> */}
 
         <div className={styles.coursesContainer}>
           {courses.map((course: { _id: string, course_name: string }) => (
